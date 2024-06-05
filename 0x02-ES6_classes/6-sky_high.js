cff4c24ff2ed4dc0dae3169deada568b1680f631
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-06-05 13:15:24 +0000

    ne

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,6 +1,16 @@
-import SkyHighBuilding from './6-sky_high.js';
+import Building from './5-building.js';
 
-const building = new SkyHighBuilding(140, 60);
-console.log(building.sqft);
-console.log(building.floors);
-console.log(building.evacuationWarningMessage());
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
