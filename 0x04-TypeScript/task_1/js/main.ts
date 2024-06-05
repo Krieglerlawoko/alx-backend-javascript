c8913c93f98fbc1ad27f199d6685fa79eea6261f
Krieglerlawoko <132452940+Krieglerlawoko@users.noreply.github.com>
2024-06-05 21:38:36 +0300

    Update main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// task_1/js/main.ts
+
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any; // To allow additional properties
+}
+
+// Example usage
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+
+console.log(teacher3);
