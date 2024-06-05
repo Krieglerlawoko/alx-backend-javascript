44648b68f1c8893caa2ec2e3e620c06a214ecd06
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-06-05 13:09:34 +0000

    new

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,28 +1,13 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-export default class Car {
+class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
-  }
-
-  get brand() {
-    return this[_brand];
-  }
-
-  get motor() {
-    return this[_motor];
-  }
-
-  get color() {
-    return this[_color];
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    // Create a new Car object with the same attributes
-    return new Car(this[_brand], this[_motor], this[_color]);
+    // Create a new Car instance with the same attributes
+    const carClone = new Car(this._brand, this._motor, this._color);
+    return carClone;
   }
 }
