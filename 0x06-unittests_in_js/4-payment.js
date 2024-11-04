215399f0358e803af88f925699360f672dd38d50
Krieglerlawoko <132452940+Krieglerlawoko@users.noreply.github.com>
2024-11-04 16:35:06 +0300

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/4-payment.js
+++
@@ -1,8 +0,0 @@
-const { calculateNumber } = require('./utils');
-
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const result = calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${result}`);
-}
-
-module.exports = sendPaymentRequestToApi;
