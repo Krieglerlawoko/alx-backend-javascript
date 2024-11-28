ed0b983ebf12b38a0dd3162ee3942e7061f05dea
Kriegler Lawoko <krieglerlawokojudge@gmail.com>
2024-11-28 11:15:56 +0000

    new

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,28 +1,26 @@
 import fs from 'fs';
 
-const readDatabase = (filePath) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-                return;
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        const result = {};
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        lines.shift(); // Remove header row
+
+        for (const line of lines) {
+            const [firstname, , , field] = line.split(',');
+            if (!result[field]) {
+                result[field] = [];
             }
+            result[field].push(firstname);
+        }
 
-            const result = {};
-            const lines = data.split('\n').filter((line) => line.trim() !== '');
-            lines.shift(); // Remove header row
-
-            for (const line of lines) {
-                const [firstname, , , field] = line.split(',');
-                if (!result[field]) {
-                    result[field] = [];
-                }
-                result[field].push(firstname);
-            }
-
-            resolve(result);
-        });
+        resolve(result);
     });
-};
+});
 
 export default readDatabase;
