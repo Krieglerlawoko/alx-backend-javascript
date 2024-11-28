958a60a0bc9f05698d5335617a6b3d499468d35d
Kriegler Lawoko <krieglerlawokojudge@gmail.com>
2024-11-28 11:52:43 +0000

    new

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,26 +1,27 @@
 import fs from 'fs';
 
 const readDatabase = (filePath) => new Promise((resolve, reject) => {
-	fs.readFile(filePath, 'utf8', (err, data) => {
-		if (err) {
-			reject(err);
-			return;
-		}
-		
-		const result = {};
-		const lines = data.split('\n').filter((line) => line.trim() !== '');
-		lines.shift(); // Remove header row
-		
-		for (const line of lines) {
-			const [firstname, , , field] = line.split(',');
-			if (!result[field]) {
-				result[field] = [];
-			}
-			result[field].push(firstname);
-		}
-		
-		resolve(result);
-	});
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return reject(err);
+    }
+
+    const result = {};
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    // Skip the header row
+    lines.shift();
+
+    lines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!result[field]) {
+        result[field] = [];
+      }
+      result[field].push(firstname);
+    });
+
+    resolve(result);
+  });
 });
 
 export default readDatabase;
